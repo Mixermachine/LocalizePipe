949cb9a0175813927e8ac3645575a85eeecc5826
Aaron Dietz <aaron.dietz@rubean.com>
2026-02-18 23:30:17 +0100

    Renames resource bundle to align with project name

--- src/main/kotlin/de/aarondietz/localizepipe/LocalizePipeBundle.kt
+++ src/main/kotlin/de/aarondietz/localizepipe/LocalizePipeBundle.kt
@@ -3,10 +3,10 @@ package de.aarondietz.localizepipe
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.PropertyKey
 
-private const val BUNDLE = "messages.MyMessageBundle"
+private const val BUNDLE = "messages.LocalizePipeBundle"
 
-internal object MyMessageBundle {
-    private val instance = DynamicBundle(MyMessageBundle::class.java, BUNDLE)
+internal object LocalizePipeBundle {
+    private val instance = DynamicBundle(LocalizePipeBundle::class.java, BUNDLE)
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any?): String {
